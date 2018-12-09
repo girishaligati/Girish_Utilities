367417bffa0367a2b2998163f1dbe45fc5a62db4
Girish Aligati <girishaligati@gmail.com>
2018-12-09 22:56:03 +0530

    2: After Exception Handling

--- File_Sorting_with_dates.py
+++ File_Sorting_with_dates.py
@@ -1,5 +1,6 @@
 #Program for sorting the files based on the files names that has dates in it of type : "IMG_20180522_115409,VID_20180706_172806"
     #Update 1 : Only Windows files, written for oneplus6 photo format
+    #Update 2 : Adding code for exception handling when the file in dest already exists
 import os
 from datetime import datetime as dt
 import shutil as sh
@@ -19,8 +20,10 @@ for dir,dirs,files in os.walk(abs_path):
             str_mkdir=dt.strptime(date_str_from_file[1],'%Y%m%d').strftime('%m-%B %Y')
             if(str_mkdir in os.listdir(dest_path)):
                 print("Copying "+files[i]+" now into the folder " + str_mkdir )
-                sh.move(files[i],dest_path+'\\'+str_mkdir)
+                try:
+                    sh.move(files[i],dest_path+'\\'+str_mkdir)
+                except Exception as e:
+                    print(e)
             else:
                 print("Directory doesn't exist, creating now")
                 os.mkdir(dest_path+'\\'+str_mkdir) 
-                sh.move(files[i],dest_path+'\\'+str_mkdir)
